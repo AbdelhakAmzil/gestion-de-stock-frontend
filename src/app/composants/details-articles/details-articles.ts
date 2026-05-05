c04f470d9d5f1289b16c0ed78ae2e808313c2f45
Abdelhak AMZIL <abdelhakamzil.2000@gmail.com>
2026-05-05 18:38:55 +0100

    coding models party

--- src/app/composants/details-articles/details-articles.ts
+++ src/app/composants/details-articles/details-articles.ts
@@ -1,40 +1,48 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
+//import { ArticleDto } from '../../../gs-api/src/models/article-dto';
+//import { ArticleService } from '../../services/article/article.service';
 
 @Component({
   selector: 'app-details-articles',
   standalone: false,
   templateUrl: './details-articles.html',
-  styleUrl: './details-articles.css',
+  styleUrls: ['./details-articles.css'],
 })
-
 export class DetailsArticles implements OnInit {
 
-  //@Input()
-  //articleDto: ArticleDto = {};
-  //@Output()
-  //suppressionResult = new EventEmitter();
+  //@Input() articleDto: ArticleDto = {};
+  @Output() suppressionResult = new EventEmitter<string>();
+
+  showModal = false;
 
   constructor(
     private router: Router,
     //private articleService: ArticleService
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onImgError(event: any): void {
+    event.target.src = 'assets/product.png';
   }
 
-  /*modifierArticle(): void {
-    this.router.navigate(['nouvelarticle', this.articleDto.id]);
+  modifierArticle(): void {
+    //this.router.navigate(['nouvelarticle', this.articleDto.id]);
   }
 
   confirmerEtSupprimerArticle(): void {
-    if (this.articleDto.id) {
-      this.articleService.deleteArticle(this.articleDto.id)
-        .subscribe(res => {
-          this.suppressionResult.emit('success');
-        }, error => {
-          this.suppressionResult.emit(error.error.error);
-        });
-    }
-  }*/
+    /*if (!this.articleDto.id) return;
+
+    this.articleService.deleteArticle(this.articleDto.id).subscribe({
+      next: () => {
+        this.showModal = false;
+        this.suppressionResult.emit('success');
+      },
+      error: (err) => {
+        this.showModal = false;
+        this.suppressionResult.emit(err?.error?.error ?? 'Erreur inconnue');
+      }
+    });*/
+  }
 }
